3c40f1ef9a0e83430e7a824c2870cc507c0b2bf4
Thai <nguyenvanthai0212@gmail.com>
2019-09-09 17:19:58 +0700

    add part 2

--- 2_simple_pg.py
+++ 2_simple_pg.py
@@ -11,74 +11,70 @@ def mlp(x, sizes, activation=tf.nn.tanh, output_activation=None):
     return tf.layers.dense(x, units=sizes[-1], activation=output_activation)
 
 
-def train(env_name='CarPole-V0', hidden_sizes=[32], lr=1e-2,
-          epochs=200, batch_size=5000, render=False):
+def reward_to_go(rews):
+    n = len(rews)
+    rtgs = np.zeros_like(rews)
+    for i in reversed(range(n)):
+        rtgs[i] = rews[i] + (rtgs[i + 1] if i + 1 < n else 0)
+    return rtgs
+
+
+def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
+          epochs=500, batch_size=5000, render=False):
     env = gym.make(env_name)
     obs_dim = env.observation_space.shape[0]
     n_acts = env.action_space.n
-
-    # core of policy network
+    # make core of policy network
     obs_ph = tf.placeholder(shape=(None, obs_dim), dtype=tf.float32)
     logits = mlp(obs_ph, sizes=hidden_sizes + [n_acts])
-    # make action selection op
     actions_0 = tf.multinomial(logits=logits, num_samples=1)
     actions = tf.squeeze(actions_0, axis=1)
 
-    # make loss function
     weights_ph = tf.placeholder(shape=(None,), dtype=tf.float32)
     act_ph = tf.placeholder(shape=(None,), dtype=tf.int32)
     action_masks = tf.one_hot(act_ph, n_acts)
     log_probs = tf.reduce_sum(action_masks * tf.nn.log_softmax(logits), axis=1)
     loss = - tf.reduce_mean(weights_ph * log_probs)
 
-    # make train op
     train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
 
     def train_one_epoch():
-        # make some empty lists for logging
         batch_obs = []
         batch_acts = []
         batch_weights = []
         batch_rets = []
         batch_lens = []
 
-        # reset episode variables
         obs = env.reset()
         done = False
         ep_rews = []
 
-        finished_rendering_this_epoch = False
+        finished_rendering_this_episode = False
         while True:
-            if (not finished_rendering_this_epoch) and render:
+            if (not finished_rendering_this_episode) and render:
                 env.render()
             batch_obs.append(obs.copy())
-            # act in the environment
-            act_0, act = sess.run([actions_0, actions], {obs_ph: obs.reshape(1, -1)})
-            # print('action', act_0, act)
-            act = act[0]
+            act = sess.run(actions, {obs_ph: obs.reshape(1, -1)})[0]
             obs, rew, done, _ = env.step(act)
 
-            # save action, reward
             batch_acts.append(act)
             ep_rews.append(rew)
-            # save info about the episode if the episode finished
+
             if done:
                 ep_ret, ep_len = sum(ep_rews), len(ep_rews)
                 batch_rets.append(ep_ret)
                 batch_lens.append(ep_len)
 
-                batch_weights += [ep_ret] * ep_len
-
+                batch_weights += list(reward_to_go(ep_rews))
                 obs, done, ep_rews = env.reset(), False, []
-
-                finished_rendering_this_epoch = True
+                finished_rendering_this_episode = True
 
                 if len(batch_obs) > batch_size:
                     break
 
-        batch_loss, _ = sess.run([loss, train_op], {
+        batch_loss, _ = sess.run([loss, train_op], feed_dict={
             obs_ph: np.array(batch_obs),
             act_ph: np.array(batch_acts),
             weights_ph: np.array(batch_weights)
@@ -90,7 +86,7 @@ def train(env_name='CarPole-V0', hidden_sizes=[32], lr=1e-2,
         if i > 180:
             render = True
         print('epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f' % (
-        i, batch_loss, np.mean(batch_rets), np.mean(batch_lens)))
+            i, batch_loss, np.mean(batch_rets), np.mean(batch_lens)))
 
 
 train('CartPole-v1', render=False, lr=0.01)
